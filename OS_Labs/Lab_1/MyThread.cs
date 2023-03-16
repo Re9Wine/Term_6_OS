f4888d97581cd7ca31b593cf3b52c9d82a63ec41
Re9Wine <asckortan@gmail.com>
2023-03-16 13:57:20 +0300

    Create MyThread.cs

---
+++ OS_Labs/Lab_1/MyThread.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab_1
+{
+    internal class MyThread
+    {
+        public int count;
+        public int repetitionCount;
+        public Thread thrd;
+        static bool stop = false;
+        static string currentName;
+
+        public MyThread(string name, int repetitionCount)
+        {
+            count = 0;
+            thrd = new Thread(new ThreadStart(this.run));
+            thrd.Name = name;
+            currentName = name;
+            this.repetitionCount = repetitionCount;
+        }
+
+        void run()
+        {
+            Console.WriteLine("Поток " + thrd.Name + " стартовал. ");
+            do
+            {
+                count++;
+            } while (stop == false && count < repetitionCount);
+            stop = true;
+        }
+
+    }
+}
